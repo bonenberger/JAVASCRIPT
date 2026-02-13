dd4fcea7c9601ff642a2ae1721092ab4788cb8df
Rafael Bonenberger <118410209+bonenberger@users.noreply.github.com>
2026-02-13 06:08:07 -0300

    aulas 10 11 e 12

---
+++ aula11/ex09.js
@@ -0,0 +1,12 @@
+var vel = 60
+console.log(`a velocidade do seu carro é ${vel}km/h`)
+console.log('você ultrapassou o limite de velocidade, MULTADO')
+console.log('dirija sempre utilizando o cinto de segurança')
+
+var vel = 60
+console.log(`a velocidade do seu carro é ${vel}km/h`)
+if (vel > 60) {
+   console.log('você ultrapassou o limite de velocidade, MULTADO')
+}
+
+console.log('dirija sempre utilizando o cinto de segurança')
---
+++ aula11/ex10.js
@@ -0,0 +1,6 @@
+var nac = 'brasil'
+if (nac == "brasil") {
+    console.log('você é brasileiro')
+}else {
+    console.log('você é estrangeiro')
+}

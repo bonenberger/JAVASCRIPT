ae99add81c06fcac5021d5ff4cacaab202c6388b
Rafael Bonenberger <118410209+bonenberger@users.noreply.github.com>
2026-02-13 12:16:44 -0300

    resolucao do exercícios

--- exercicio/ex14/script.js
+++ exercicio/ex14/script.js
@@ -1,1 +1,16 @@
-var msg = window.document.getElementById('msg')
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var foto = window.document.getElementById('img')
+    var data = new Date()
+    var atual = data.getHours()
+    msg.innerHTML = `agora são ${atual} horas`
+    if (atual >= 0 && atual < 12){
+        foto.src = "imagens/manha.jpg"
+    }else if (atual >= 12 && atual < 18) {
+        foto.src = "imagens/tarde.jpg"
+    }else {
+        foto.src = "imagens/noite.jpg"
+    }
+}
+
+

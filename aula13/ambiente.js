f79e6d37f3e20f3ad67a1ffc66da9acba71b2e22
Rafael Bonenberger <118410209+bonenberger@users.noreply.github.com>
2026-02-14 07:38:31 -0300

    aula 12 sobre while

---
+++ aula13/ambiente.js
@@ -0,0 +1,30 @@
+/*
+var c = 1
+while (c <= 6) {
+    console.log(`contei ${c}`)
+    c++
+}
+*/
+var c = 1
+do {
+    console.log(`passo ${c}`)
+    c++
+}while (c <= 6)
+
+
+
+
+
+
+
+
+
+
+/*
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+*/
---
+++ aula13/node_96603a9ed9dd3.tmp.js
@@ -0,0 +1,29 @@
+/*
+var c = 1
+while (c <= 6) {
+    console.log(`contei ${c}`)
+    c++
+}
+*/
+var c = 1
+do {
+    console.log(`passo ${c}`)
+}while (c <= 6)
+
+
+
+
+
+
+
+
+
+
+/*
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+*/
